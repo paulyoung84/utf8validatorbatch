3c608fb5815d8d959489d1427333bb2dd0e2307c
paulyoung84 <paulyoung84@hotmail.co.uk>
2019-12-19 11:13:19 +0000

    changes os.system for subprocess.run

--- utf8batchvalidator.py
+++ utf8batchvalidator.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import glob
+import subprocess
 
 droidfile = input("Please enter filepath of DROID CSV: ") #this line lets you state the DROID CSV which contains files you want to validate identifying as extension
 output = os.path.basename(os.path.normpath(droidfile))
@@ -29,10 +30,10 @@ for file in extension: #runs validator over files, if filename already exists it
 		fileoutput = fileoutput + "[" + str(count) + "]"
 		command = "validate.bat " + '"' + file + '"'+ " > " + output + "\\" + '"' + fileoutput +'"' + ".txt"
 		count += 1
-		os.system(command)
+		subprocess.run(command)
 	else:
 		command = "validate.bat " + '"' + file + '"'+ " > " + output + "\\" + '"' + fileoutput +'"' + ".txt"
-		os.system(command)
+		subprocess.run(command)
 	
 
 for file in glob.glob(output+"/*"): #generates a list of all files in output folder, if it contains invalid UTF8 characters it will print the output filename to the console, as well as add to a list which will be exported to a csv file called errorfiles.csv create in bin directory
